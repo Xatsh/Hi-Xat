885064df6f6f10e067883bc0a809bff8af7de07a
Xat <i@xatloon.com>
2024-10-08 16:24:10 +0800

    feat: add misans font support

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,14 @@
-/** @type {import('astro').AstroConfig} */
-
 import tailwind from '@astrojs/tailwind'
+import font from 'vite-plugin-font'
 
+/** @type {import('astro').AstroUserConfig} */
 export default {
   integrations: [tailwind()],
+  vite: {
+    plugins: [
+      font.vite({
+        scanFiles: ['src/**/*.{astro,mdx,ts,tsx}'],
+      }),
+    ],
+  },
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-
 import { getIconCollections, iconsPlugin as IconsPlugin } from '@egoist/tailwindcss-icons'
 import TypographyPlugin from '@tailwindcss/typography'
 
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,mdx,ts,tsx}'],
   darkMode: 'class',
