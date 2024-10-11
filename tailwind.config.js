d8b946648ebad05340061323fc86513288dc1660
Xat <i@xatloon.com>
2024-10-11 14:40:19 +0800

    style: adjust color and typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,6 @@ import TypographyPlugin from '@tailwindcss/typography'
 import colors from 'tailwindcss/colors'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
-const paper = {
-  50: '#FBF8EF',
-  100: '#F5EFDB',
-  200: '#ECE0BB',
-  300: '#DFC990',
-  400: '#D0AC5D',
-  500: '#C48E40',
-  600: '#B17234',
-  700: '#98582F',
-  800: '#7C482C',
-  900: '#643A26',
-  950: '#422214',
-}
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,mdx,tsx}'],
@@ -38,15 +24,14 @@ export default {
           dark: colors.gray[700],
           DEFAULT: colors.gray[200],
         },
-        mute: colors.gray[500],
-        paper,
+        mute: colors.slate[500],
         primary: {
-          dark: colors.blue[600],
-          DEFAULT: paper[600],
+          dark: colors.sky[400],
+          DEFAULT: colors.amber[600],
         },
         secondary: {
           dark: colors.gray[800],
-          DEFAULT: paper[100],
+          DEFAULT: colors.amber[100],
         },
       },
       fontFamily: {
